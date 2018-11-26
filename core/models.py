8376ded5839dcdd8a1c3753e5040b693d12c79fa
cat9563 <crysatl9563@gmail.com>
2018-11-26 17:51:57 -0500

    adds models and migrates to db
    Co-authored-by: stevelknievel marks.steven@gmail.com

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE,
+            blank=True, null=True)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    url = models.URLField()
+    description = models.CharField(max_length=255)
+
+class Comment(models.Model):
+    commenter = models.ForeignKey(to=User, on_delete=models.CASCADE,
+            blank=True, null=True)
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
+
+class Vote(models.Model):
+    voter = models.ForeignKey(to=User, on_delete=models.CASCADE,
+            blank=True, null=True)
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (
+            'voter',
+            'post',
+        )

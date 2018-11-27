a0cd814e450374f8a4607503db114afc4d140fcc
stevelknievel <marks.steven@gmail.com>
2018-11-26 22:13:55 -0500

    Login / Logout / Register Created Super User Created Created index.html and base.html in order to add registration / login to nav.  Only functionality achieved.  Not pretty. Created templates folder Created templates/registration folder Created login.html, logout.html, regsitration_complete.html, registration_form.html Created urls for home and accounts in URLS.py Created view for Index.html (home page) Created PW change, reset files and functionality
    **Used “user.username” convention in the PW reset files, so need to make sure that squares with what we’re calling users in the models.
    
    !NEED TO FIX PASSWORD RESET!
    
    co-authored-by Crystal Ramirez <crystal9563@gmail.com>

---
+++ apile/urls.py
@@ -0,0 +1,36 @@
+"""apile URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# from core.backends import MyRegistrationView -- this is part of having people create "something"
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,)
+from django.contrib import admin
+from django.urls import path, include
+from core import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('accounts/password/reset/', PasswordResetView, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
+    path('accounts/password/reset/done/', PasswordResetDoneView, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
+    path('accounts/password/reset/<uidb64>/<token>/', PasswordResetConfirmView, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
+    path('accounts/password/done/', PasswordResetCompleteView,
+    {'template_name': 'registration/password_reset_complete.html'},
+    name="password_reset_complete"),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('admin/', admin.site.urls),
+]

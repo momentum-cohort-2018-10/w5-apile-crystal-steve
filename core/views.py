857566eb2564162b0b60c46cfa7c2d6e89f20ddc
cat9563 <crysatl9563@gmail.com>
2018-12-01 10:42:49 -0500

    updates views with bulma, started working with votes

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.template.defaultfilters import slugify
 from django.shortcuts import render, redirect
+from django.db.models import Count
 from core.forms import PostForm, CommentForm
-from core.models import Post, Comment, Votes
+from core.models import Post, Comment, Vote
+from django.contrib import messages
+
 
 
 def index(request):
@@ -75,3 +78,25 @@ def create_comment(request, slug):
         'post': post,
         'form': form,
     })
+
+@login_required
+def favorites_list(request):
+     posts = request.user.vote_set_posts.all()
+     return render_post_list(request, 'my favorite posts', posts)
+
+def vote_button(request, slug):
+    """
+    gets the post that will be voted on 
+    """
+
+    post = Post.objects.get(slug=slug)
+    if post in request.voter.vote_set.all():
+        post.vote_set.get(user=request.user).delete()
+        #message = f"you have taken back your vote"
+    else:
+        post.vote_set.create(user=request.user)
+        #message = f"you have just  up voted {post.title}."
+
+    #messages.add_message(request, messages.INFO, message)
+    #return redirect(f'/#post-{post.slug}')
+    return render('thanks dude')
